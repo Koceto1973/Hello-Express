4c5d06f2d964688b36d2b2103fb528caf5cadee3
TeamK <koceto1973@yahoo.ca>
2019-01-01 23:10:43 +0200

    creating web server

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// middleware example, static assets are just that, they will always be the same between requests,
+// looks in the public folder for a file that matches the name of the request path (i.e. help.html). 
+// If it finds it, then it serves that, if not then it sends the request to the route handlers.
+app.use(express.static(__dirname + '/public'));
+
 // response sent as html, express takes care of other  response elements
 app.get('/', (req, res) => res.send('<h1>Hello Express!</h1>'));
 
@@ -17,4 +22,7 @@ app.get('/detail', (req, res) => {
 
 
 // listener set up
-app.listen(port, () => console.log(`Hello Express app, listening on port ${port}!`));
+app.listen(port, () => {
+    // specific tasks by callback, when the server is up and listening
+    console.log(`Hello Express app, listening on port ${port}!`)
+});
