4da1a389df78ea0423df4ad2d479667812be1feb
TeamK <koceto1973@yahoo.ca>
2019-01-02 07:59:41 +0200

    express middlewares used for site-maintenance and activity logger

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs');
 const port = 3000;
 const app = express();
 
 const hbs = require('hbs');
 
+// middleware, example of catching all routes 
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');  // no data injection here
+//   // no next() here, app blocks here
+// });
+
 // middleware example with static routes ( no modified res based on req )
 app.use(express.static(__dirname + '/staticAssets')); // if route is here, send it as a response
 
-// middleware, example catching all non-static routes 
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs');  // no data injection here
-// });
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 
-hbs.registerPartials(__dirname + '/views/partials')
+// middleware example for site activity logger
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (error)=>{ 
+    if (error) {
+      console.log('Unable to append the server log!');
+    }
+  });
+  next();
+});
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
@@ -41,7 +58,7 @@ app.get('/projects', (req, res) => {
   });
 });
 
-// unknown route sends back json with errorMessage
+// middleware, unknown route sends back json with errorMessage
 app.use((req, res, next) => {
   res.send({
     errorMessage: 'Unable to handle request'
